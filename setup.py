70da9279f8e109413614c58de678fc453c01b7c9
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-11-02 14:37:50 +0900

    Add python script to setup symlink

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import os
+
+CURRENT_PATH = os.getcwd()
+HOME = os.environ["HOME"]
+XDG_CONFIG_HOME = ""
+try:
+    XDG_CONFIG_HOME = os.environ["XDG_CONFIG_HOME"]
+except KeyError as e:
+    XDG_CONFIG_HOME = HOME + "/.config"
+
+print("$CURRENT_PATH: " + CURRENT_PATH)
+print("$HOME: " + HOME)
+print("$XDG_CONFIG_HOME: " + XDG_CONFIG_HOME)
+
+LINK = [
+    ("/gitconfig",           HOME + "/.gitconfig"),
+    ("/gitignore_global",    HOME + "/.gitignore_global"),
+]
+
+# Remove current exist links.
+for (_, link) in LINK:
+    try:
+        os.remove(link)
+        print("Remove: " + link)
+    except OSError as e:
+        print(e)
+
+# Make links
+for (src, link) in LINK:
+    src = CURRENT_PATH + src
+    print("Create Symlink (link -> src): " + link + " -> " + src)
+
+    try:
+        os.symlink(src, link)
+    except OSError as e:
+        print(e)
